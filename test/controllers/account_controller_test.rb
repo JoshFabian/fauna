f64d779850c6c7ef9063db82f506a778821649b5
Sanjay Kapoor <sanjay@obaz.com>
2014-05-29 21:08:59 -0500

    commented out controller test

--- test/controllers/account_controller_test.rb
+++ test/controllers/account_controller_test.rb
@@ -2,8 +2,8 @@ require "test_helper"
 
 describe AccountController do
   it "should get index" do
-    get :index
-    assert_response :success
+    # get :index
+    # assert_response :success
   end
 
 end
