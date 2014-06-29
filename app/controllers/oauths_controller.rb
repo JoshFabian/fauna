eae85723d73c49097484a1c277d75ce57d1a8d57
Sanjay Kapoor <sanjay@obaz.com>
2014-06-29 11:32:24 -0500

    updated cloudinary config

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -8,20 +8,23 @@ class OauthsController < ApplicationController
     @oauth = Oauth.from_omniauth(session["omniauth.auth"])
     if @oauth.persisted?
       if user_signed_in?
+        logger.post("tegu.app", log_data.merge({event: 'facebook.connect'}))
         # user connected their facebook account
         redirect_to(user_verify_path(current_user))
       else
+        logger.post("tegu.app", log_data.merge({event: 'facebook.login'}))
         # user exists, sign them in
         @user = @oauth.user
         sign_in(:user, @user)
         redirect_to(after_sign_in_path_for(@user)) and return
       end
     else
+      logger.post("tegu.app", log_data.merge({event: 'facebook.signup'}))
       # redirect
       redirect_to(new_facebook_signup_path) and return
     end
   end
-  
+
   # GET /auth/failure
   def failure
     redirect_to(root_path)
