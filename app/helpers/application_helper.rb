e795f8caa1bf103de3cc7a89b007a922fc63e8b5
Sanjay Kapoor <sanjay@obaz.com>
2014-07-01 16:46:58 -0500

    updated helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,7 +19,7 @@ module ApplicationHelper
   def listing_image_tile(image)
     cloudinary_url(image.full_public_id, transformation: [{width: 235, height: 200, crop: 'fill'}])
   rescue Exception => e
-    "http://www.placehold.it/235x200"
+    "http://www.placehold.it/235x200/fff&text=Image"
   end
 
   def listing_shipping_prices_options(listing, options={})
