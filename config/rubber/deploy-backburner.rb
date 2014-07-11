7dad459c0fe7a9240a42f840fe99404b213be83d
Sanjay Kapoor <sanjay@obaz.com>
2014-07-10 19:34:51 -0500

    added deploy-backburner.rb

---
+++ config/rubber/deploy-backburner.rb
@@ -0,0 +1,27 @@
+namespace :rubber do
+
+  namespace :backburner do
+
+    desc "Stops the backburner service"
+    task :stop, :roles => :app, :on_error => :continue do
+      rsudo "kill `cat /mnt/tegu-#{Rubber.env}/shared/pids/backburner.pid`"
+    end
+
+    desc "Starts the backburner service"
+    task :start, :roles => :app do
+      rsudo "cd /mnt/tegu-#{Rubber.env}/current && bundle exec backburner -d -P /mnt/tegu-#{Rubber.env}/shared/pids/backburner.pid -l /mnt/tegu-#{Rubber.env}/shared/log/backburner.log"
+    end
+
+    desc "Restarts the backburner service"
+    task :restart, :roles => :app do
+      stop
+      start
+    end
+
+    desc "Reloads the backburner service"
+    task :reload, :roles => :app do
+      restart
+    end
+  end
+
+end
