ca14344264a03668f4aef7e59145ab6599931afa
Sanjay Kapoor <sanjay@obaz.com>
2014-07-12 19:51:27 -0500

    udpated beanstalkd

--- config/rubber/deploy-beanstalkd.rb
+++ config/rubber/deploy-beanstalkd.rb
@@ -9,6 +9,16 @@ namespace :rubber do
     task :install, :roles => :app do
       rsudo "apt-get install beanstalkd"
     end
+
+    desc "Stops the beanstalkd server"
+    task :stop, :roles => :app do
+      rsudo "service beanstalkd stop; exit 0"
+    end
+
+    desc "Starts the beanstalkd server"
+    task :start, :roles => :app do
+      rsudo "service beanstalkd status || service beanstalkd start"
+    end
   end # beanstalkd
 
 end
