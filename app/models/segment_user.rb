cc7e2a38fcc0e6ae627e8fdb838e6f71ac3c1367
Sanjay Kapoor <sanjay@obaz.com>
2014-07-17 10:40:54 -0500

    updated segment user

--- app/models/segment_user.rb
+++ app/models/segment_user.rb
@@ -6,7 +6,7 @@ class SegmentUser
     raise Exception, "test environment" if Rails.env.test?
     hash = {user_id: user.id, traits: {name: user.full_name, email: user.email, created_at: user.created_at}}
     result = Analytics.identify(hash)
-    logger.post("tegu.app", log_data.merge({event: 'segmentio.identify', user_id: user.id}))
+    logger.post("tegu.app", log_data.merge({event: 'segmentio.user_identify', user_id: user.id}))
     result
   rescue Exception => e
     false
@@ -16,8 +16,8 @@ class SegmentUser
 
   def self.track_signup(user)
     raise Exception, "test environment" if Rails.env.test?
-    result = track(user_id: user.id, event: 'signup')
-    logger.post("tegu.app", log_data.merge({event: 'segmentio.signup', user_id: user.id}))
+    result = track(user_id: user.id, event: 'user_signup')
+    logger.post("tegu.app", log_data.merge({event: 'segmentio.user_signup', user_id: user.id}))
     result
   rescue Exception => e
     false
