53437a2af2f1fac3a9fd8af7d8d1b500a7030a2e
Sanjay Kapoor <sanjay@obaz.com>
2014-07-19 10:28:41 -0500

    fixed shipping local bug

--- app/assets/javascripts/jquery_validator_helpers.js
+++ app/assets/javascripts/jquery_validator_helpers.js
@@ -2,6 +2,10 @@ jQuery.validator.addMethod("price", function(value, element) {
   return this.optional(element) || value.replace(/[,$]/g, '') > 0.0;
 }, "Please enter a valid price");
 
+jQuery.validator.addMethod("any-price", function(value, element) {
+  return this.optional(element) || value.replace(/[,$]/g, '') >= 0.0;
+}, "Please enter a valid price");
+
 jQuery.validator.addMethod("state_code", function(value, element) {
   return this.optional(element) || value.match(/^[A-Z]{2,2}$/);
 }, "Please enter a valid state code");
