4280229142ad5e5c3e700b454a776b67633654e2
Sanjay Kapoor <sanjay@obaz.com>
2014-07-24 18:52:57 -0500

    reverted session_store change

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Tegu::Application.config.session_store :cookie_store, key: '_tegu_session', domain: ".fauna.net"
+Tegu::Application.config.session_store :cookie_store, key: '_tegu_session'
