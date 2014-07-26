71cd85fe55c62bad4208e8c3839d0ff53d0b0b2d
Sanjay Kapoor <sanjay@obaz.com>
2014-07-26 14:05:54 -0500

    cleanup

--- lib/handle_route.rb
+++ lib/handle_route.rb
@@ -4,7 +4,8 @@ class HandleRoute
   # end
 
   def matches?(request)
-    return false if request.path.match(/^\/(about|auth|categories|invites|landing|listings|listing_reports|login|logout|messages|payments|paypal|plans|reviews|signup|sms|stories|tags|users|waitlists)/)
+    s = "(about|auth|categories|invites|landing|listings|listing_reports|login|logout|messages|payments|paypal|plans|reviews|signup|sms|stories|tags|users|waitlists)"
+    return false if request.path.match(/^\/#{s}/)
     true
   end
 end
