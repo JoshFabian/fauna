7eb24215258b9a41446b6e27c7b158ee276d3bce
Sanjay Kapoor <sanjay@obaz.com>
2014-07-27 16:46:53 -0500

    added test email job

---
+++ app/jobs/test_email_job.rb
@@ -0,0 +1,16 @@
+class TestEmailJob
+  include Backburner::Queue
+  include Loggy
+  queue "emails"  # defaults to 'backburner-jobs' tube
+  queue_priority 1000 # most urgent priority is 0
+  queue_respond_timeout 100 # number of seconds before job times out
+
+  def self.perform(hash)
+    email = hash['email']
+    subject = hash['subject']
+    TestMailer.email(email, subject: subject).deliver
+    1
+  rescue Exception => e
+    0
+  end
+end
