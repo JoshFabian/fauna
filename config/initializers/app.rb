38b296c092d78bf01131d3ae3161eba79702bc92
Sanjay Kapoor <sanjay@obaz.com>
2014-08-01 22:41:10 -0500

    updated email test settings

--- config/initializers/app.rb
+++ config/initializers/app.rb
@@ -19,17 +19,23 @@ end
 Settings.resolve!
 
 Tegu::Application.configure do
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: Settings[Rails.env][:email_host] }
-  config.action_mailer.smtp_settings = {
-    address: 'smtp.mandrillapp.com',
-    port: 587,
-    domain: 'fauna.net',
-    user_name: Settings[Rails.env][:mandrillapp_username],
-    password: Settings[Rails.env][:mandrillapp_password],
-    enable_starttls_auto: false,
-    openssl_verify_mode: "none"
-    # authentication: :login,
-  }
+  if Rails.env.test?
+    config.action_mailer.delivery_method = :test
+    config.action_mailer.raise_delivery_errors = false
+    config.action_mailer.default_url_options = { host: Settings[Rails.env][:email_host] }
+  else
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.raise_delivery_errors = true
+    config.action_mailer.default_url_options = { host: Settings[Rails.env][:email_host] }
+    config.action_mailer.smtp_settings = {
+      address: 'smtp.mandrillapp.com',
+      port: 587,
+      domain: 'fauna.net',
+      user_name: Settings[Rails.env][:mandrillapp_username],
+      password: Settings[Rails.env][:mandrillapp_password],
+      enable_starttls_auto: false,
+      openssl_verify_mode: "none"
+      # authentication: :login,
+    }
+  end
 end
