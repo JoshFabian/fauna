7f1f381ac057fa63f6f117215dea22fd08049d73
Sanjay Kapoor <sanjay@obaz.com>
2014-08-11 17:37:23 -0500

    enable facebook_share feature in production for listings

--- config/initializers/paddock.rb
+++ config/initializers/paddock.rb
@@ -1,8 +1,8 @@
 include Paddock
 
 Paddock(Rails.env) do
+  enable :story_facebook_share
   disable :listing_card, in: [:production]
-  disable :story_facebook_share, in: [:production]
   disable :backburner, in: [:test]
   disable :backburner_emails, in: [:test]
 end
