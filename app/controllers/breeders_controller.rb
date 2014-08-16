2b4b8dfaf22c3080a3f5d335238c7f01573d7a14
Sanjay Kapoor <sanjay@obaz.com>
2014-08-16 07:06:40 -0500

    changed breeders#index to use search query

--- app/controllers/breeders_controller.rb
+++ app/controllers/breeders_controller.rb
@@ -2,10 +2,8 @@ class BreedersController < ApplicationController
 
   # GET /breeders
   def index
-    # query = {filter: {term: {breeder: 1}}, sort: {id: 'desc'}}
-    # @breeders = User.search(query).page(params[:page]).per(per).records
-
-    @breeders = User.breeder.order("id desc").page(params[:page]).per(per)
+    query = {filter: {term: {breeder: 1}}, sort: {id: 'desc'}}
+    @breeders = User.search(query).page(params[:page]).per(per).records
   end
 
   protected
