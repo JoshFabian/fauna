cce3b853b772e9b058c6f06fceeb56e6e4f42eb8
Sanjay Kapoor <sanjay@obaz.com>
2014-08-19 12:39:22 -0500

    added fluent::logger error method

--- config/initializers/fluent.rb
+++ config/initializers/fluent.rb
@@ -1,3 +1,16 @@
 require 'fluent-logger'
+
+# add error method for undefined method erorrs
+module Fluent
+  module Logger
+
+    def self.error(*args)
+      # noop
+    end
+
+  end # logger
+end # fluent
+
 # singleton
 Fluent::Logger::FluentLogger.open(nil, :format_json => true, :host=>'localhost', :port=>24224)
+
